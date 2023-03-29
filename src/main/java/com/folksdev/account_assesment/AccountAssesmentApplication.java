5807a7e4f80d7b766a556c91045bb7df45de5590
Batuhan <speedburn34@hotmail.com>
2023-03-29 14:35:57 +0300

    SpringBoot projesi olusturuldu, Paket yapisi olusturuldu, Kotlin entegrasyonu yapildi, JPA Modelleri duzenlendi

---
+++ src/main/java/com/folksdev/account_assesment/AccountAssesmentApplication.java
@@ -0,0 +1,13 @@
+package com.folksdev.account_assesment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountAssesmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountAssesmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/folksdev/account_assesment/AccountAssesmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.folksdev.account_assesment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountAssesmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
